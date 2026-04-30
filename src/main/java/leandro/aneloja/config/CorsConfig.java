3f18ea995670ef3ab0eaf4f3497d44c7f91fe960
Your Name <you@example.com>
2026-04-30 19:10:54 -0300

    save

--- src/main/java/leandro/aneloja/config/CorsConfig.java
+++ src/main/java/leandro/aneloja/config/CorsConfig.java
@@ -15,9 +15,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
